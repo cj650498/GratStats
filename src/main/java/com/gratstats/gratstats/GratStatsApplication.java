40aa67c8fe009747afc0d788ea2cd05ce79f1b64
Caleb Jones <cj650498@icloud.com>
2024-03-14 09:46:41 -0400

    Configured application properties for GratStats
    - Set the spring application name to GratStats.
    - Configured SSL settings including key store path, password, type, alias, and port.
    - Set up DataSource with MySQL including URL, username, and password.
    - Configured server.servlet.session.cookie.same-site to 'strict'.

---
+++ src/main/java/com/gratstats/gratstats/GratStatsApplication.java
@@ -0,0 +1,13 @@
+package com.gratstats.gratstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GratStatsApplication.class, args);
+    }
+
+}
