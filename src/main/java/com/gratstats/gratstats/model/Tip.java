f088caa093a131e299492c90bbbd4379cade826d
Caleb Jones <cj650498@icloud.com>
2024-03-14 10:52:05 -0400

    Add model package and classes

--- src/main/java/com/gratstats/gratstats/model/Tip.java
+++ src/main/java/com/gratstats/gratstats/model/Tip.java
@@ -1,2 +1,72 @@
-package com.gratstats.gratstats.model;public class Tip {
+package com.gratstats.gratstats.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Entity class representing a Tip in the application.
+ */
+@Entity
+@Table(name = "Tip")
+public class Tip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TipId")
+    private int id;
+
+    @Column(name = "MonetaryValue")
+    private BigDecimal monetaryValue;
+
+    @Column(name = "Date")
+    private Date date;
+
+    // Establishing a many-to-one relationship between Tip and User entities
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "UserId")
+    private User user;
+
+    public Tip() {
+    }
+
+    public Tip(BigDecimal monetaryValue, Date date, User user) {
+        this.monetaryValue = monetaryValue;
+        this.date = date;
+        this.user = user;
+    }
+
+    // Getters and setters...
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BigDecimal getMonetaryValue() {
+        return monetaryValue;
+    }
+
+    public void setMonetaryValue(BigDecimal monetaryValue) {
+        this.monetaryValue = monetaryValue;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/gratstats/gratstats/model/User.java
+++ src/main/java/com/gratstats/gratstats/model/User.java
@@ -1,2 +1,115 @@
-package com.gratstats.gratstats.model;public class User {
+package com.gratstats.gratstats.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Entity class representing a User with a role in the application.
+ */
+@Entity
+@Table(name = "User")
+public class User {
+
+    public enum Role {
+        ADMIN, KITCHEN, CASHIER, BOTH
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserId")
+    private int id;
+
+    @NotBlank(message = "First Name is required")
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @NotBlank(message = "Last Name is required")
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "UserName")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Column(name = "Password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Role is required")
+    @Column(name = "Role")
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Tip> tips = new ArrayList<>();
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String username, String password, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+     public List<Tip> getTips() {
+        return tips;
+     }
+
+     public void setTips(List<Tip> tips) {
+        this.tips = tips;
+     }
 }
